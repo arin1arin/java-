3f6122fbfed9688e75afb3b2c33fed003f7c0739
Arintech1992 <136111683+arin1arin@users.noreply.github.com>
2025-01-17 23:53:43 +0530

    Add files via upload

---
+++ prime_between_two_no.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class prime_between_two_no {
+  public static void main(String[] args) {
+    int n1,n2,i,j;
+    System.out.println("Enter two Number..");
+    Scanner sc=new Scanner(System.in);
+    n1=sc.nextInt();
+    n2=sc.nextInt();
+
+    for( i=n1; i<=n2;i++)
+    {
+      for ( j=2; j<=i; j++)
+      {
+        if(i%j==0)
+        break;
+      }
+      if(i==j)
+      System.out.println(j);
+
+    }
+  }
+}
